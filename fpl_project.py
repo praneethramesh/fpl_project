1e0bdb1fdaab45e84a979bc541654c64113f74aa
Praneeth Ramesh <praneethramesh@outlook.com>
2021-02-26 05:00:38 -0500

    Update fpl_project.py

--- fpl_project.py
+++ fpl_project.py
@@ -6,29 +6,47 @@ import time
 
 @st.cache
 def get_FPL_data():
-    df = pd.read_csv("FPL20-GW20.csv")
-    return df.set_index("Team")
+    df = pd.read_csv("/Users/praneethramesh/Downloads/FPL20-GW20.csv")
+    return df.set_index('Surname')
 
 st.title('FPL Statistics WebApp')
-df = get_FPL_data()
-team = st.multiselect(
-    "Choose teams", list(df.index), ["ARS", "LIV"]
-)
-if not team:
-    st.error("Please select at least one team.")
+
+df=get_FPL_data()
+
+st.sidebar.subheader('Filters')
+#Sidebar Options
+params={
+'Team' : st.sidebar.selectbox('Team',df['Team'].unique()),
+'PositionsList' : st.sidebar.selectbox('PositionsList',('FWD','MID','DEF','GLK')),
+'Cost' : st.sidebar.slider('Max Cost',3800000,max(df['Cost']),step=10000)
+}
+
+
+def map_df(df):
+    df=df[df['Team']==params['Team']]
+    df=df[df['PositionsList']==params['PositionsList']]
+    df=df[df['Cost']<params['Cost']]
+    df.reset_index()
+    return df
+
+def run_data():
+    df1=map_df(df)
+    df1
+
+btn = st.sidebar.button("Apply")
+if btn:
+    run_data()
 else:
-    data = df.loc[team]
-    st.write("### FPL Database", data.sort_index())
+    pass
 
-    data = data.T.reset_index()
 
-expander = st.beta_expander("FAQ")
-expander.write("To be Filled in Due Time")
+#expander = st.beta_expander("FAQ")
+#expander.write("To be Filled in Due Time")
 
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button('Created By')
-if pressed:
-    right_column.write("Darshil Prajapati & Praneeth Ramesh")
+#left_column, right_column = st.beta_columns(2)
+#pressed = left_column.button('Created By')
+#if pressed:
+#    right_column.write("Darshil Prajapati & Praneeth Ramesh")
 
 
 
