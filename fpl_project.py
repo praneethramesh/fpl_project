413b3b194584e80f44e6437de9df3e4899c38a65
Praneeth Ramesh <praneethramesh@outlook.com>
2021-03-05 04:28:16 -0500

    Update fpl_project.py

--- fpl_project.py
+++ fpl_project.py
@@ -2,36 +2,46 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
+import plotly_express as px
 import time
 
 @st.cache
 def get_FPL_data():
     df = pd.read_csv("FPL20-GW20.csv")
-    return df.set_index('Surname')
+    return df.set_index('FirstName')
 
 st.title('FPL Statistics WebApp')
+
+st.markdown('Choose the teams and positions for which you want to scout players for FPL')
 
 df=get_FPL_data()
 
 st.sidebar.subheader('Filters')
 #Sidebar Options
 params={
-'Team' : st.sidebar.selectbox('Team',df['Team'].unique()),
+'Team' : st.sidebar.multiselect('Team',df['Team'].unique()),
 'PositionsList' : st.sidebar.selectbox('PositionsList',('FWD','MID','DEF','GLK')),
-'Cost' : st.sidebar.slider('Max Cost',3800000,max(df['Cost']),step=10000)
+'Cost' : st.sidebar.slider('Max Cost',3800000,max(df['Cost'])+500000,step=10000)
 }
 
 
 def map_df(df):
-    df=df[df['Team']==params['Team']]
+    if params['Team'] != "":
+        df=df[df['Team'].isin(params['Team'])]
+    else:
+        st.write('Dam Dam Dam')
     df=df[df['PositionsList']==params['PositionsList']]
-    df=df[df['Cost']<params['Cost']]
+    df=df[df['Cost']<=params['Cost']]
     df.reset_index()
     return df
 
 def run_data():
     df1=map_df(df)
     df1
+    fig = px.scatter(df1, x ='Cost',y='AveragePoints',color='Surname')
+    st.plotly_chart(fig)
+
+
 
 btn = st.sidebar.button("Apply")
 if btn:
@@ -47,7 +57,3 @@ else:
 #pressed = left_column.button('Created By')
 #if pressed:
 #    right_column.write("Darshil Prajapati & Praneeth Ramesh")
-
-
-
-        
